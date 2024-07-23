ad1d3a1d4170a0712f24b4271cc38ba5c434ef6f
traanfnhaatjminh <minh12a2nvc@gmail.com>
2024-07-23 22:00:06 +0700

    update management
    cashier feature

--- src/main/java/pl/codeleak/demos/sbt/controller/ManagementController.java
+++ src/main/java/pl/codeleak/demos/sbt/controller/ManagementController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import pl.codeleak.demos.sbt.model.Category;
 import pl.codeleak.demos.sbt.model.Product;
 import pl.codeleak.demos.sbt.service.CategoryService;
@@ -22,10 +23,19 @@ public class ManagementController {
 
     @GetMapping("/management")
     public String management(Model model) {
-        List<Product> listP = productService.getLastestProducts();
+        Iterable<Product> listP = productService.getAllProducts();
         Iterable<Category> listC = categoryService.getAllCategories();
         model.addAttribute("categories", listC);
         model.addAttribute("products", listP);
+        return "management";
+    }
+
+    @GetMapping("/management/products/category/{cid}")
+    public String productByCategory(@PathVariable int cid, Model model) {
+        Iterable<Product> listP = productService.getProductsByCategory(cid);
+        Iterable<Category> listC = categoryService.getAllCategories();
+        model.addAttribute("products", listP);
+        model.addAttribute("categories", listC);
         return "management";
     }
 }
