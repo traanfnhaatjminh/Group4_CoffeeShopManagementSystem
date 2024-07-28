32ba5f9be93b5d59798ff7bb92856861fde181fd
nthemanhvjp <nthemanh1005@gmail.com>
2024-07-29 00:44:11 +0700

    a
    a

--- src/main/java/pl/codeleak/demos/sbt/Application.java
+++ src/main/java/pl/codeleak/demos/sbt/Application.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
