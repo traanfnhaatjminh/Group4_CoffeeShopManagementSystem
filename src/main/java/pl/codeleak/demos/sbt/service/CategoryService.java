c5585b0625b41bcaf4821c9995b38f0388693666
Khailsqhe176149 <khailsqhe176149@fpt.edu.vn>
2024-08-08 10:08:13 +0700

    FE

--- src/main/java/pl/codeleak/demos/sbt/service/CategoryService.java
+++ src/main/java/pl/codeleak/demos/sbt/service/CategoryService.java
@@ -1,3 +1,52 @@
+//package pl.codeleak.demos.sbt.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import pl.codeleak.demos.sbt.model.Category;
+//import pl.codeleak.demos.sbt.repository.CategoryRepository;
+//
+//import java.text.Normalizer;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Locale;
+//import java.util.regex.Pattern;
+//
+//@Service
+//public class CategoryService {
+//
+//    private final CategoryRepository categoryRepository;
+//    private final ProductService productService;
+//
+//    @Autowired
+//    public CategoryService(CategoryRepository categoryRepository, ProductService productService) {
+//        this.categoryRepository = categoryRepository;
+//        this.productService = productService;
+//    }
+//
+//    public Iterable<Category> getAllCategories() {
+//        return categoryRepository.findAll();
+//    }
+//
+//    public Category getCategoryById(int cid) {
+//        return categoryRepository.findById(cid).orElse(null);
+//    }
+//
+//    public void saveCategory(Category category) {
+//        categoryRepository.save(category);
+//    }
+//
+//    public void deleteCategoryById(int id) {
+//        productService.deleteProductsByCategoryId(id);
+//        categoryRepository.deleteById(id);
+//    }
+//
+//    public void updateCategory(Category category) {
+//        categoryRepository.save(category);
+//    }
+//
+//
+//}
+
 package pl.codeleak.demos.sbt.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,44 +54,34 @@ import org.springframework.stereotype.Service;
 import pl.codeleak.demos.sbt.model.Category;
 import pl.codeleak.demos.sbt.repository.CategoryRepository;
 
-import java.text.Normalizer;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.regex.Pattern;
 
 @Service
 public class CategoryService {
 
-    private final CategoryRepository categoryRepository;
-    private final ProductService productService;
-
     @Autowired
-    public CategoryService(CategoryRepository categoryRepository, ProductService productService) {
-        this.categoryRepository = categoryRepository;
-        this.productService = productService;
-    }
+    private CategoryRepository categoryRepository;
 
-    public Iterable<Category> getAllCategories() {
+    public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
-    public Category getCategoryById(int cid) {
-        return categoryRepository.findById(cid).orElse(null);
-    }
-
-    public void saveCategory(Category category) {
+    public void save(Category category) {
         categoryRepository.save(category);
     }
 
-    public void deleteCategoryById(int id) {
-        productService.deleteProductsByCategoryId(id);
-        categoryRepository.deleteById(id);
+    public void update(Category category) {
+        Category existingCategory = categoryRepository.findById(category.getCid()).orElse(null);
+        if (existingCategory != null) {
+            existingCategory.setGroupName(category.getGroupName());
+            existingCategory.setCategoryName(category.getCategoryName());
+            existingCategory.setDescribe(category.getDescribe());
+            categoryRepository.save(existingCategory);
+        }
     }
 
-    public void updateCategory(Category category) {
-        categoryRepository.save(category);
+    public void deleteById(int cid) {
+        categoryRepository.deleteById(cid);
     }
-
-
 }
+
