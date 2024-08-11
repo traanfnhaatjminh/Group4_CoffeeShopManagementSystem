2af4a5e1af36b56e6399cddf6bcd50159cc70993
Khailsqhe176149 <khailsqhe176149@fpt.edu.vn>
2024-08-11 15:25:26 +0700

    FixEncodePassword

--- src/main/java/pl/codeleak/demos/sbt/configs/WebConfiguration.java
+++ src/main/java/pl/codeleak/demos/sbt/configs/WebConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -47,7 +48,10 @@ public class WebConfiguration extends WebSecurityConfigurerAdapter implements We
     }
 
     @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
